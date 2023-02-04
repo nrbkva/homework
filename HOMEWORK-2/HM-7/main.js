56b52259d1393c5bd0ab3d74b9a00d8675fc4e9d
nrbkva <nnnurbekovva@gmail.com>
2023-02-04 14:39:00 +0600

    HM-7

--- HOMEWORK-2/HM-7/main.js
+++ HOMEWORK-2/HM-7/main.js
@@ -1,13 +1,29 @@
-const blockTitle = document.querySelector('.title')
-const blockBody = document.querySelector('.body')
 
-// const URL = 'https://jsonplaceholder.typicode.com/posts'
-const bodyTitle = () => {
+
+const main = document.querySelector('.main .container')
+
+// function getData () {
     fetch('https://jsonplaceholder.typicode.com/posts')
-                .then(resp => resp.json())
-                .then(posts => {
-                    blockTitle.innerHTML = posts.title
-                    blockBody.innerHTML = posts.body
-                })
+        .then((res) => res.json())
+        .then((data) => {
+            const div = document.createElement('div')
+            data.forEach((data) => {
+                 const div = document.createElement('div')
+                    div.innerHTML += `
+                        <div class="main__card">
+                            <div class="image">
+                                <img src="images/work-img.jpg" alt="logo"/>
+                             </div>
+                            <div class="card">
+                                <h2>${data.title}</h2>
+                                <p>${data.body}</p>
+                            </div>
+                        </div>
+                `
 
-}
+                main.append(div)
+                div.setAttribute('class', 'main__inner')
+            })
+        })
+// }
+// getData()
